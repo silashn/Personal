2e849c6d6e6eba3cae4bce5149bcbb2a6e36d83f
Silas <silashn@kragghc.com>
2019-01-16 19:45:28 +0100

    Added Admin pages and edited _Layout.cshtml to support admin menu.

---
+++ Playground/Pages/Pages.Tools/Http/UrlHelper.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Pages.Tools.Http
+{
+    public static class UrlHelper
+    {
+        public static bool IsAdmin(HttpContext Context)
+        {
+            return Context.Request.Path.HasValue && Context.Request.Path.Value.ToLower().Contains("admin");
+        }
+    }
+}
---
+++ Playground/Pages/Pages.Web/Pages/Admin/Membership/Themes.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Pages.Web.Pages.Admin.Membership
+{
+    public class ThemesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
