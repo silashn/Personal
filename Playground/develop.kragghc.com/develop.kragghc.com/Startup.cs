14ccc1dc4b7dd8a841f92294c0240c5c2760483a
Silas <silashn@kragghc.com>
2019-03-28 10:43:00 +0100

    Added test files for test domains.

---
+++ Playground/develop.kragghc.com/develop.kragghc.com/Startup.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace develop.kragghc.com
+{
+    public class Startup
+    {
+        public IConfiguration config { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                .AddJsonFile("Appsettings/appsettings.json", false, true)
+                .AddJsonFile($"~/AppSettings/appsettings.{env.EnvironmentName}.json", true, true)
+                .AddEnvironmentVariables();
+
+            config = builder.Build();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.BuildServiceProvider();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+
+            app.UseStaticFiles();
+            app.UseMvc(options =>
+            {
+                options.MapRoute("Default", "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
